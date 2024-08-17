9e2ef62461ffa73d7e71530cf5baac1db7b46805
R3truong <129334913+R3truong@users.noreply.github.com>
2024-08-16 20:52:06 -0500

    Database Integration Complete
    -Database now properly displays on frontend
    -Major hurdle: Database has delay at the start because of multiple functions

--- vtuber-database.js
+++ vtuber-database.js
@@ -1,5 +1,7 @@
 const { Client } = require("pg");
 const express = require("express");
+const cors = require("cors");
+Math.random();
 
 const client = new Client({
   host: "localhost",
@@ -8,29 +10,53 @@ const client = new Client({
   password: "password",
   database: "vtuberdatabase",
 });
+let jsonVtuber;
 const app = express(),
   port = 3000;
-
+app.use(cors());
+let id;
 client.connect();
 
-app.get("api/vtuber", (req, res) => {
-  client.query('SELECT * from vtuber WHERE "ID" = $1', (error, result) => {
-    if (error) {
-      console.error("Error querying database: ", error.stack);
-      result.status(500).send("Server Error");
-    } else {
-      const vtuberData = result.rows[0];
-      res.json({
+// client.query('SELECT * from vtuber WHERE "ID" = 1');
+
+app.get("/vtuber", (req, res) => {
+  id = client.query(`SELECT * FROM vtuber`, (err, res) => {
+    id = Math.floor(Math.random() * res.rowCount) + 1;
+  });
+  client.query(`SELECT * from vtuber WHERE "ID" = ${id}`, (err, res) => {
+    if (!err) {
+      const vtuberData = res.rows[0];
+      jsonVtuber = {
         name: vtuberData.Name,
         agency: vtuberData.Agency,
         channel: vtuberData.Channel,
         face: vtuberData.Face,
         body: vtuberData.Body,
-      });
+      };
+    } else {
+      console.log(err.message);
     }
+    client.end;
   });
+  res.send(jsonVtuber);
 });
 
 app.listen(port, () => {
   console.log(`Server running at: http://localhost:${port}`);
 });
+
+//   client.query('SELECT * from vtuber WHERE "ID" = 1', (error, result) => {
+//     if (error) {
+//       console.error("Error querying database: ", error.stack);
+//     } else {
+//       const vtuberData = result.rows[0];
+//       let vtuberJson = {
+//         name: vtuberData.Name,
+//         agency: vtuberData.Agency,
+//         channel: vtuberData.Channel,
+//         face: vtuberData.Face,
+//         body: vtuberData.Body,
+//       };
+//       res.send(JSON.stringify(vtuberJson));
+//     }
+//   });
--- vtuber.js
+++ vtuber.js
@@ -1,11 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("api/vtuber")
+document.addEventListener("DOMContentLoaded", async (e) => {
+  let nameHTML;
+  let agencyHTML;
+  let bodyHTML;
+  let faceHTML;
+  e.preventDefault();
+  fetch("http://localhost:3000/vtuber", {
+    method: "GET",
+  })
     .then((response) => response.json())
     .then((data) => {
-      const vtuber_name = document.getElementsByClassName("vtuber_name");
-      vtuber_name.textContent = data.name;
-      console.log(vtuber_name);
-      console.log(data.name);
+      console.log(data);
+      nameHTML = document.getElementById("vtuber-name");
+      agencyHTML = document.getElementById("vtuber-agency");
+      bodyHTML = document.getElementById("vtuber-body");
+      faceHTML = document.getElementById("vtuber-face");
+      nameHTML.innerText = data.name;
+      agencyHTML.innerText = data.agency;
+      bodyHTML.alt = `${data.name}'s full body`;
+      bodyHTML.src = data.body;
+      faceHTML.alt = `${data.name}'s face`;
+      faceHTML.src = data.face;
     })
     .catch((error) => {
       console.log(error);
